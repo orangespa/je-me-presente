8eeff6d42dbc9dc7dc4c5d550b3fafaaf8ba66f1
hong <luu.bichhong@gmail.com>
2017-07-27 11:00:44 +0200

    improve routes.rb et css fichier

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  # get 'static_pages/home'
+
+  get '/contact', to: 'static_pages#contact' 
+
+  get '/about', to:'static_pages#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
