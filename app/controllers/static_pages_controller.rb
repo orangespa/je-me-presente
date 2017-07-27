5ac88bc375049c15347465eb8bdf0b7b658c290f
hong <luu.bichhong@gmail.com>
2017-07-27 11:40:43 +0200

    ajouter pages bout me et about my binome , créer le lien entre about et about me & about my binome, insérer le partial footer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def about_me
+  end
+
+  def about_mybinome
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # get 'static_pages/home'
-
+  root 'static_pages#home'
   get '/contact', to: 'static_pages#contact' 
 
   get '/about', to:'static_pages#about'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  get '/about/me', to: 'static_pages#about_me', as: 'about_me'
+  get '/about/my_binome', to: 'static_pages#about_mybinome', as: 'about_mybinome'
+  
 end
